bdbb4ae548c629a0872c7366f37008e25af2c4c8
Atsushi Odagiri <atsushi.odagiri@open-c.jp>
2018-07-09 15:52:37 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="eqassertions",
-    version="0.1",
-    author="Atsushi Odagiri",
-    author_email="aodagx@gmail.com",
-    license="MIT",
-)
+setup()
