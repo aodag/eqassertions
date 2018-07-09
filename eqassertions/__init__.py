666e859bda3c3c0c0b7b912dc09bf7a333f8b3dc
Atsushi Odagiri <atsushi.odagiri@open-c.jp>
2018-07-09 15:44:36 +0900

    first commit

---
+++ eqassertions/__init__.py
@@ -0,0 +1,40 @@
+__version__ = '0.1'
+
+
+class Any:
+    """
+    >>> v = {"a": 1, "b": 2}
+    >>> assert v == {"a": 1, "b": Any()}
+    """
+    def __eq__(self, o):
+        return True
+
+
+class NotNone:
+    """
+    >>> v = {"a": 1, "b": 2}
+    >>> assert v == {"a": 1, "b": NotNone()}
+    """
+    def __eq__(self, o):
+        return o is not None
+
+
+class IsTrue:
+    """
+    >>> v = {"a": 1, "b": 2}
+    >>> assert v == {"a": 1, "b": IsTrue()}
+    """
+    def __eq__(self, o):
+        return bool(o)
+
+
+class IsA:
+    """
+    >>> v = {"a": 1, "b": 2}
+    >>> assert v == {"a": 1, "b": IsA(int)}
+    """
+    def __init__(self, typ):
+        self.typ = typ
+
+    def __eq__(self, o):
+        return isinstance(o, self.typ)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="eqassertions",
+    version="0.1",
+    author="Atsushi Odagiri",
+    author_email="aodagx@gmail.com",
+    license="MIT",
+)
