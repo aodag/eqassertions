d2750eaa6faa784c89293775a4d35a96924ee011
Atsushi Odagiri <atsushi.odagiri@open-c.jp>
2018-07-11 12:24:43 +0900

    new features
    - added Len assertion
    - improve repr methods

--- eqassertions/__init__.py
+++ eqassertions/__init__.py
@@ -2,14 +2,22 @@ import re
 
 __version__ = '0.2'
 
+marker = object()
+
 
 class Any:
     """
     >>> v = {"a": 1, "b": 2}
     >>> assert v == {"a": 1, "b": Any()}
     """
+    o = marker
+
     def __eq__(self, o):
+        self.o = o
         return True
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else 'Any'
 
 
 class NotNone:
@@ -17,8 +25,18 @@ class NotNone:
     >>> v = {"a": 1, "b": 2}
     >>> assert v == {"a": 1, "b": NotNone()}
     """
+    o = marker
+
     def __eq__(self, o):
-        return o is not None
+        e = o is not None
+        if e:
+            self.o = o
+        else:
+            self.o = "%s is None" % o
+        return e
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else 'NotNone'
 
 
 class IsTrue:
@@ -26,8 +44,18 @@ class IsTrue:
     >>> v = {"a": 1, "b": 2}
     >>> assert v == {"a": 1, "b": IsTrue()}
     """
+    o = marker
+
     def __eq__(self, o):
-        return bool(o)
+        e = bool(o)
+        if e:
+            self.o = o
+        else:
+            self.o = "%s == False" % o
+        return e
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else 'IsTrue'
 
 
 class IsA:
@@ -35,11 +63,21 @@ class IsA:
     >>> v = {"a": 1, "b": 2}
     >>> assert v == {"a": 1, "b": IsA(int)}
     """
+    o = marker
+
     def __init__(self, typ):
         self.typ = typ
 
     def __eq__(self, o):
-        return isinstance(o, self.typ)
+        e = isinstance(o, self.typ)
+        if e:
+            self.o = o
+        else:
+            self.o = "type(%s) == %s is not %s" % (o, type(o), self.typ)
+        return e
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else ('IsA(%s)' % self.typ)
 
 
 class Match:
@@ -51,8 +89,37 @@ class Match:
      ...
     AssertionError
     """
+    o = marker
+
     def __init__(self, pattern):
         self.pt = re.compile(pattern)
 
     def __eq__(self, o):
-        return self.pt.match(str(o))
+        e = self.pt.match(str(o))
+        if e:
+            self.o = o
+        else:
+            self.o = "%s is not match %s" % (o, self.pt.pattern)
+        return e
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else ('Match(%s)' % self.pt.pattern)
+
+
+class Len:
+    o = marker
+
+    def __init__(self, l):
+        self.l = l
+
+    def __eq__(self, o):
+        e = len(o) == self.l
+        if e:
+            self.o = o
+        else:
+            self.o = "len(%s) = %d != %d" % (o, len(o), self.l)
+        return e
+
+    def __repr__(self):
+        return repr(self.o) if self.o != marker else 'Len'
+
---
+++ tests/test_it.py
@@ -0,0 +1,143 @@
+import pytest
+
+
+class TestAny:
+    @pytest.fixture
+    def target(self):
+        from eqassertions import Any
+        return Any
+
+    def test_eq(self, target):
+        a = target()
+        assert "abcde" == a
+
+    def test_repr(self, target):
+        a = target()
+        assert repr(a) == "Any"
+
+    def test_repr_after_eq(self, target):
+        a = target()
+        a == "abcde"
+        assert repr(a) == "'abcde'"
+
+
+class TestNotNone:
+    @pytest.fixture
+    def target(self):
+        from eqassertions import NotNone
+        return NotNone
+
+    def test_eq(self, target):
+        n = target()
+        assert n == "a"
+
+    def test_eq_none(self, target):
+        n = target()
+        assert not n == None
+
+    def test_repr(self, target):
+        n = target()
+        assert repr(n) == 'NotNone'
+
+    def test_repr_after_eq(self, target):
+        n = target()
+        n == "abcde"
+        assert repr(n) == "'abcde'"
+
+    def test_repr_after_eq_fail(self, target):
+        n = target()
+        n == None
+        assert repr(n) == "'None is None'"
+
+
+class TestIsTrue:
+    @pytest.fixture
+    def target(self):
+        from eqassertions import IsTrue
+        return IsTrue
+
+    def test_eq(self, target):
+        t = target()
+        assert t == "a"
+
+    def test_eq_false(self, target):
+        t = target()
+        assert not t == ""
+
+    def test_repr(self, target):
+        t = target()
+        assert repr(t) == "IsTrue"
+
+    def test_repr_after_eq(self, target):
+        t = target()
+        t == "a"
+        assert repr(t) == "'a'"
+
+    def test_repr_after_false(self, target):
+        t = target()
+        t == []
+        assert repr(t) == "'[] == False'"
+
+
+class TestIsA:
+    @pytest.fixture
+    def target(self):
+        from eqassertions import IsA
+        return IsA
+
+    def test_eq(self, target):
+        class Dummy:
+            pass
+
+        a = target(Dummy)
+        assert a == Dummy()
+
+    def test_eq_not_instance(self, target):
+        class Dummy:
+            pass
+
+        a = target(Dummy)
+        assert not a == 1
+        
+    def test_repr(self, target):
+        a = target(int)
+        assert repr(a) == "IsA(<class 'int'>)"
+
+    def test_repr_after_eq(self, target):
+        a = target(int)
+        a == 1
+        assert repr(a) == "1"
+
+    def test_repr_after_eq_fail(self, target):
+        a = target(int)
+        a == "1"
+        assert repr(a) == '"type(1) == <class \'str\'> is not <class \'int\'>"'
+
+
+class TestMatch:
+    @pytest.fixture
+    def target(self):
+        from eqassertions import Match
+        return Match
+
+    def test_eq(self, target):
+        m = target(r"\d")
+        assert m == "1"
+
+    def test_eq_not_match(self, target):
+        m = target(r"\d")
+        assert not m == "a"
+
+    def test_repr(self, target):
+        m = target(r"\d")
+        assert repr(m) == "Match(\\d)"
+
+    def test_repr_after_eq(self, target):
+        m = target(r"\d")
+        m == "1"
+        assert repr(m) == "'1'"
+
+    def test_repr_after_eq_not_match(self, target):
+        m = target(r"\d")
+        m == "a"
+        assert repr(m) == r"'a is not match \\d'"
